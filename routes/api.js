aa251138bc3a1beecec1f415ec55c005244945b7
elmer.torres.mgx <elmer.torres.mgx@gmail.com>
2022-10-19 15:59:25 +0800

    add vercel json file

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,15 @@
 var router = require("express").Router();
 
-router.get("/playlists", function (req, res) {
-    res.json({ foo: "sample express jwfs" });
+router.get("/playlists", async (req, res) => {
+    try {
+        res.json({
+            status: 200,
+            message: "Get data has successfully",
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
 });
 
 module.exports = router;
